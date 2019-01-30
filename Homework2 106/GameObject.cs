265d31086b263b0db86116ff240980448532cf5a
Bobby <18levrob@gmail.com>
2019-01-30 12:28:50 -0500

    GameObject class used to represent all objects in the game

---
+++ Homework2 106/GameObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Homework2_106
+{
+	class GameObject
+	{
+		private Texture2D texture;
+		private Rectangle position;
+
+		/// <summary>
+		/// An Object storing an image, it's size, and position (in a rectangle object)
+		/// </summary>
+		/// <param name="texture"></param>
+		/// <param name="rectangle"></param>
+		public GameObject(Texture2D texture, Rectangle rectangle)
+		{
+			this.texture = texture;
+			this.position = rectangle;
+		}
+
+		/// <summary>
+		/// Image used to show the object
+		/// </summary>
+		public Texture2D Texture { get { return texture;} set { texture = value; } }
+		/// <summary>
+		/// Rectangle storing the position and size of the object
+		/// </summary>
+		public Rectangle Position { get { return position; } set { position = value;} }
+
+		/// <summary>
+		/// The x coordinate of the top-left of this Object
+		/// </summary>
+		public int X
+		{
+			get { return position.X; }
+			set
+			{
+				position.Location = new Point(value, position.Y);
+			}
+		}
+		/// <summary>
+		/// The y coordinate of the top-left of this Object
+		/// </summary>
+		public int Y
+		{
+			get { return position.Y; }
+			set
+			{
+				position.Location = new Point(position.X, value);
+			}
+		}
+
+		public virtual void Draw(SpriteBatch sb)
+		{
+			sb.Draw(texture, position, Color.White);
+		}
+	}
+}
